6ff840538c393ba4c72bba5a1d1e170dee34fae7
wuyukang <wuyukang@gmail.com>
2017-01-15 13:01:35 +0800

    no message

---
+++ debianxp.sh
@@ -0,0 +1,12 @@
+####debian 6 xp install vnc without xfce
+apt-get update
+apt-get install vnc4server qemu -y
+vncserver
+chmod +x ~/.vnc/xstartup
+echo 'qemu  -hda winxp.img -m 512M -net nic,model=virtio -net user -redir tcp:3389::3389'>>/root/.vnc/xstartup
+wget http://darkwo.googlecode.com/files/vncserver
+cp vncserver /etc/init.d/
+chmod +x /etc/init.d/vncserver
+update-rc.d vncserver defaults
+wget -O winxp.img http://down.senra.me/laozuo-xp.img
+reboot
