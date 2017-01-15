df3e160e8ee05bcc8262097984eb05d16687b2df
KenGe <WuYuKang@users.noreply.github.com>
2017-01-15 13:25:34 +0800

    Update debianxp.sh
    update

--- debianxp.sh
+++ debianxp.sh
@@ -4,8 +4,9 @@ apt-get install vnc4server qemu -y
 vncserver
 chmod +x ~/.vnc/xstartup
 echo 'qemu  -hda winxp.img -m 512M -net nic,model=virtio -net user -redir tcp:3389::3389'>>/root/.vnc/xstartup
+wget https://raw.githubusercontent.com/WuYuKang/VNCSERVER-XP-DEBIAN/vncserver
 cp vncserver /etc/init.d/
 chmod +x /etc/init.d/vncserver
 update-rc.d vncserver defaults
-wget -O winxp.img http://down.senra.me/laozuo-xp-10G.img
+wget -O winxp.img http://down.senra.me/laozuo-xp.img
 reboot
