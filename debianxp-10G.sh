d4f83a0aa8b879aa393742ee13bdcf2dd0419f71
Yorfir <yorfir@users.noreply.github.com>
2017-01-15 13:51:42 +0800

    Update debianxp-10G.sh

--- debianxp-10G.sh
+++ debianxp-10G.sh
@@ -4,7 +4,7 @@ apt-get install vnc4server qemu -y
 vncserver
 chmod +x ~/.vnc/xstartup
 echo 'qemu  -hda winxp.img -m 512M -net nic,model=virtio -net user -redir tcp:3389::3389'>>/root/.vnc/xstartup
-wget https://raw.githubusercontent.com/WuYuKang/VNCSERVER-XP-DEBIAN/vncserver
+wget https://raw.githubusercontent.com/yorfir/VNCSERVER-XP-DEBIAN/vncserver
 cp vncserver /etc/init.d/
 chmod +x /etc/init.d/vncserver
 update-rc.d vncserver defaults
